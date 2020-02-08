d52d7fc407084c448eaa8377efee8376570f2ac1
satwika <satwika@android3iioy7>
2020-02-09 00:19:33 +0530

    done

--- maven.demo/src/test/java/com/epam/maven/demo/chocolates.java
+++ maven.demo/src/test/java/com/epam/maven/demo/chocolates.java
@@ -1,5 +1,23 @@
 package com.epam.maven.demo;
 
 public class chocolates {
-
+	public int total_weight(int[] choc,int n)
+	{
+		int tot=0;
+		for(int i=0;i<n;i++)
+		{
+			tot+=choc[i];
+		}
+		return tot;
+	}
+	public int num_of_candy(int low,int high,int[] choc,int c)
+	{
+		int count=0;
+		for(int i=0;i<c;i++)
+		{
+			if(choc[i]>=low && choc[i]<=high)
+				count++;
+		}
+		return count;
+	}
 }
